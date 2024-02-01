8743163bca17fdf3101324eaa64dca29e20c4b0f
Christopher Fenn <cfenn@vwd.com>
2024-02-01 10:56:53 +0100

    compact code

--- bookstore.js
+++ bookstore.js
@@ -22,26 +22,20 @@ const packageDefinition = protoLoader.loadSync(protoPath, {
 const protoDescriptor = grpc.loadPackageDefinition(packageDefinition)
 
 const server = new grpc.Server()
-
-const methodImplementations = {
+server.addService(protoDescriptor.bookstore.Bookstore.service, {
   EchoBooks (call, callback) {
     console.log(`EchoBooks called with the following parameters:\n`, call.request, '\n')
     callback(null, {
       books: call.request.books,
     })
   },
-}
-server.addService(protoDescriptor.bookstore.Bookstore.service, methodImplementations)
+})
 
 console.log(`Starting Bookstore service`)
-server.bindAsync(
-  `0.0.0.0:${grpcPort}`,
-  grpc.ServerCredentials.createInsecure(),
-  (err) => {
+server.bindAsync(`0.0.0.0:${grpcPort}`, grpc.ServerCredentials.createInsecure(), (err) => {
     if (err) {
       throw new Error(`Could not start gRPC server on port ${grpcPort}`, err)
     }
-
     server.start()
     console.log(`Bookstore service is listening on port ${grpcPort}`)
 })
